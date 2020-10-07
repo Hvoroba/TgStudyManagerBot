651f36f965486380bee5595b2b5550f8c49176dc
Dmitriev <ni5668dm@gmail.com>
2020-10-07 19:08:59 +0300

    reworked parser to make bot more SOLID

--- messageParser.js
+++ messageParser.js
@@ -1,36 +1,38 @@
-function ParseTaskAddingMessage(text, addData, errorList) {
-    let firstIndex = 0;
-    let secondIndex = 0;
-    text += '-'
+function ParseTaskAddingMessage(text, errorList) {
+    let addData = []
+
+    let firstIndex = -1
+    let secondIndex = -1
 
     for (let i = 0; i < text.length; i++) {
-
-        if (text[i] == '-' | text[i] == '—') {
-
-            if (firstIndex == 0) {
-                if (errorList.length > 0) {
-                    return;
-                }
-                firstIndex = i
-            } else if (secondIndex == 0) {
-                secondIndex = i
-                addData[0].task = text.slice(firstIndex + 1, secondIndex).trim()
-            } else {
-                if (DateIsValid(text.slice(secondIndex + 1, text.length - 1).trim()) == true) {
-                    addData[0].deadline = text.slice(secondIndex + 1, text.length - 1).trim()
-                } else {
-                    errorList.push('Неверный формат даты')
-                }
-            }
+        if (text[i] == ':') {
+            firstIndex == -1 ? firstIndex = i : secondIndex = i
         }
     }
 
-    if (addData.deadline == null) {
-        addData.deadline = GetNextSubjectData()
+    addData.push(text.slice(0, firstIndex - 1).trim()) // Subject
+
+    if (secondIndex == -1) {
+        addData.push(text.slice(firstIndex + 1, text.length).trim()) // Task
+        addData.push('NEXT DATE') // TODO GetNextSubjectData() to find next date
+    } else {
+        addData.push(text.slice(firstIndex + 1, secondIndex - 1).trim()) // Task
+        addData.push(text.slice(secondIndex + 1, text.length).trim()) // Date
     }
+
+    if (DateIsValid(addData[2]) == false) {
+        errorList.push('Неверный ввод даты.')
+    }
+
+    if (addData[2].toUpperCase() == 'БС') addData[2] = 'Бессрочное задание'
+
+    return addData
 }
 
 function DateIsValid(dateString) {
+
+    //Бессрочная задача
+    if (dateString.toUpperCase() == 'БС') return true
 
     let naDate = dateString.slice(3, 6) + dateString.slice(0, 3) + dateString.slice(6, 10);
 
@@ -39,6 +41,8 @@ function DateIsValid(dateString) {
             return true
         }
     }
+
+    return false
 }
 
 function GetNextSubjectData(subject) {
@@ -46,12 +50,12 @@ function GetNextSubjectData(subject) {
 }
 
 function ParseSubjectEditMessage(text) {
-    let message = []
+    let addData = []
     for (let i = 0; i < text.length; i++) {
-        if (text[i] == '-' | text[i] == '—') {
-            message.push(text.slice(0, i - 1).trim())
-            message.push(text.slice(i + 1, text.length).trim())
-            return message
+        if (text[i] == ':') {
+            addData.push(text.slice(0, i - 1).trim()) //id
+            addData.push(text.slice(i + 1, text.length).trim()) //shortName
+            return addData
         }
     }
 }
